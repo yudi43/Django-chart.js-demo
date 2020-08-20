171e461349b9cb5951451215df620ee035aa722a
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-20 07:04:23 +0530

    three different ways to generate json response

--- src/charts/urls.py
+++ src/charts/urls.py
@@ -20,9 +20,8 @@ from .views import HomeView, get_data, ChartData
 
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^api/data/$', get_data, name='api-data'),
-    url(r'^api/chart/data/$', ChartData.as_view()),
-    url(r'^admin/', admin.site.urls),
-
+    url(r"^$", HomeView.as_view(), name="home"),
+    url(r"^api/data/$", get_data, name="api-data"),
+    url(r"^api/chart/data/$", ChartData.as_view()),
+    url(r"^admin/", admin.site.urls),
 ]
--- src/charts/views.py
+++ src/charts/views.py
@@ -1,18 +1,41 @@
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
+# from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.views.generic import View
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+
+
+# User = get_user_model()
+
+
+# class ChartData(APIView):
+#     authentication_classes = []
+#     permission_classes = []
+
+#     def get(self, request, format=None):
+#         qs_count = User.objects.all().count()
+#         labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
+#         default_items = [qs_count, 23, 2, 3, 12, 2]
+#         data = {
+#                 "labels": labels,
+#                 "default": default_items,
+#         }
+#         return Response(data)
+
+
 from django.http import JsonResponse
+from django.views import View
 from django.shortcuts import render
-from django.views.generic import View
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-User = get_user_model()
-
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'charts.html', {"customers": 10})
-
+        return render(request, "charts.html", {})
 
 
 def get_data(request, *args, **kwargs):
@@ -20,7 +43,7 @@ def get_data(request, *args, **kwargs):
         "sales": 100,
         "customers": 10,
     }
-    return JsonResponse(data) # http response
+    return JsonResponse(data)
 
 
 class ChartData(APIView):
@@ -28,12 +51,9 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        qs_count = User.objects.all().count()
-        labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
-        default_items = [qs_count, 23, 2, 3, 12, 2]
+        # usernames = [user.username for user in User.objects.all()]
         data = {
-                "labels": labels,
-                "default": default_items,
+            "sales": 100,
+            "customers": 10,
         }
         return Response(data)
-
