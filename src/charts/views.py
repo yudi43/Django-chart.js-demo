300217d12685586a051952803e93a417924e99fb
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-20 07:45:01 +0530

    cleanup

--- src/charts/views.py
+++ src/charts/views.py
@@ -1,36 +1,13 @@
-# from django.contrib.auth import get_user_model
-# from django.http import JsonResponse
-# from django.shortcuts import render
-# from django.views.generic import View
-
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-
-
-# User = get_user_model()
-
-
-# class ChartData(APIView):
-#     authentication_classes = []
-#     permission_classes = []
-
-#     def get(self, request, format=None):
-#         qs_count = User.objects.all().count()
-#         labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
-#         default_items = [qs_count, 23, 2, 3, 12, 2]
-#         data = {
-#                 "labels": labels,
-#                 "default": default_items,
-#         }
-#         return Response(data)
-
-
+from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+
+User = get_user_model()
 
 
 class HomeView(View):
@@ -57,5 +34,6 @@ class ChartData(APIView):
         data = {
             "sales": 100,
             "customers": 10,
+            "users": User.objects.all().count(),
         }
         return Response(data)  # third way of doing it
